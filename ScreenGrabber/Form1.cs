69c8cc00c7c44ca7bdce2201bd56636b2964f841
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2023-01-08 16:28:34 -0300

    Criado método inicial de salvar e limpar

--- ScreenGrabber/Form1.cs
+++ ScreenGrabber/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
+
 
 namespace ScreenGrabber
 {
@@ -11,9 +13,9 @@ namespace ScreenGrabber
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+        private void GrabScreen()
+        {
             Rectangle retangulo = Screen.PrimaryScreen.Bounds;
 
             Bitmap bitMap = new Bitmap(retangulo.Width, retangulo.Height);
@@ -23,9 +25,35 @@ namespace ScreenGrabber
             grafico.CopyFromScreen(0, 0, 0, 0, retangulo.Size);
 
             this.BackgroundImage = bitMap;
+        }
 
+        private void grabBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Thread.Sleep(1000);
+            GrabScreen();
+            this.Show();
+            saveBtn.Enabled =  true;
+          
+        }
 
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (this.BackgroundImage == null)
+            {
+                MessageBox.Show("No image to save!");
+            }
+            else
+            {
+                this.BackgroundImage.Save("testgrab.bpm");
+            }
+        }
 
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = null;
+            saveBtn.Enabled = false;
+        
         }
     }
 }
