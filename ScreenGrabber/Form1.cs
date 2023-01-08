7e2d24060fdb03e40fd1ace95a38ff58a69655d2
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2023-01-08 12:56:37 -0300

    ScreenGrabber já inicializado.

--- ScreenGrabber/Form1.cs
+++ ScreenGrabber/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ScreenGrabber
@@ -15,6 +9,23 @@ namespace ScreenGrabber
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Rectangle retangulo = Screen.PrimaryScreen.Bounds;
+
+            Bitmap bitMap = new Bitmap(retangulo.Width, retangulo.Height);
+
+            Graphics grafico = Graphics.FromImage(bitMap);
+
+            grafico.CopyFromScreen(0, 0, 0, 0, retangulo.Size);
+
+            this.BackgroundImage = bitMap;
+
+
+
         }
     }
 }
